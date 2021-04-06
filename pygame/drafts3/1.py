a93cc0e9768ae682744a076f25dc3c9a61d8a58c
Nurlan <78497823+nurl8n@users.noreply.github.com>
2021-04-06 19:33:24 +0600

    Create 1.py

---
+++ pygame/drafts3/1.py
@@ -0,0 +1,45 @@
+import pygame
+from pygame.locals import *
+# allows us to use the functions and variables in the pygame.locals module without having to add the lengthy pygame.locals prefix
+
+pygame.init()           # in order to use pygame library
+
+# Assign FPS a value
+FPS = 30
+FramePerSec = pygame.time.Clock()
+
+# Setting up color objects
+BLUE  = (0, 0, 255)
+RED   = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
+
+# Changes in the game are not implemented until the display.update() has been called.
+# we use for it pygame.display.update()
+# Game loop begins
+
+# Setup a 300x300 pixel display with caption
+DISPLAYSURF = pygame.display.set_mode((300, 300))
+DISPLAYSURF.fill(WHITE)
+pygame.display.set_caption("Example")
+
+# Creating Lines and Shapes
+pygame.draw.line(DISPLAYSURF, BLUE, (150, 130), (130, 170))
+pygame.draw.line(DISPLAYSURF, BLUE, (150, 130), (170, 170))
+pygame.draw.line(DISPLAYSURF, GREEN, (130, 170), (170, 170))
+pygame.draw.circle(DISPLAYSURF, BLACK, (100, 50), 30)
+pygame.draw.circle(DISPLAYSURF, BLACK, (200, 50), 30)
+pygame.draw.rect(DISPLAYSURF, RED, (100, 200, 100, 50), 2)
+pygame.draw.rect(DISPLAYSURF, BLACK, (110, 260, 80, 5))
+
+# Beginning Game Loop
+while True:
+    pygame.display.update()
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+
+    FramePerSec.tick(FPS)
